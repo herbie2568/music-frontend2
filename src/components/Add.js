1605e2352ed0f9401a07fdc7dae4cd11b427acc0
Lily Chen <chenlily1998@gmail.com>
2022-03-18 13:03:17 -0400

    merging dev into master

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,30 +3,32 @@ import React, { useState, useEffect } from 'react'
 //only need props as a param if we are passing in props to this component (we are going to here).
 const Add = (props) => {
 
-    let emptySong = { name: '', artist: '', genre: '', image: '', audio: '', price: ''}
-    const [song, setSong] = useState(emptySong)
 
-    const handleChange = (event) => {
-      setSong({ ...song, [event.target.name]: event.target.value })
-    }
+  const handleChange = (event) => {
+    setSong({ ...song, [event.target.name]: event.target.value })
+  }
 
-    const handleSubmit = (event) => {
-      event.preventDefault()
-      props.handleCreate(song)
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.handleCreate(song)
+  }
 
+
+  let emptySong = { name: '', artist: '', genre: '', image: '', audio: '' }
+  const [song, setSong] = useState(emptySong)
   return (
-    <div className = 'addForm'>
-    <h3>Add Song</h3>
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="name"></label>
-        <input className = 'addInput' type="text" name="name" value={song.name} onChange={handleChange} placeholder = 'Name...'/>
+    <div className='addForm'>
+      <h3>Add Song</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">name</label>
+        <input className='addInput' type="text" name="name" value={song.name} onChange={handleChange} placeholder='Name...' />
         <br />
         <br />
-        <label htmlFor="artist"></label>
-        <input className = 'addInput' type="text" name="artist" value={song.artist} onChange={handleChange} placeholder = 'Artist...'/>
+        <label htmlFor="artist">artist</label>
+        <input className='addInput' type="text" name="artist" value={song.artist} onChange={handleChange} placeholder='Artist...' />
         <br />
         <br />
+
 
         <label htmlFor = 'genre'></label>
         <select name = 'genre' id = 'genre' value = {song.genre} onChange = {handleChange} required>
@@ -34,26 +36,25 @@ const Add = (props) => {
             <option value = 'rock'>Rock</option>
             <option value = 'techno'>Techno</option>
             <option value = 'hiphop'>Hip-hop</option>
-
-        </select><br/><br/>
-
-        <label htmlFor="image"></label>
-        <input className = 'addInput' type="text" name="image" value={song.image} onChange={handleChange} placeholder = 'Image URL...'/>
-        <br />
-        <br />
-
-        <label htmlFor="audio"></label>
-        <input className = 'addInput' type="file" name="audio" value={song.audio} onChange={handleChange} placeholder = 'Audio file...'/>
+        </select>
+        <label for='pop'>Pop</label>
+        <label htmlFor="image">image</label>
+        <input className='addInput' type="text" name="image" value={song.image} onChange={handleChange} placeholder='Image URL...' />
         <br />
         <br />
 
         <label htmlFor="price"></label>
         <input className = 'addInput' type="text" name="price" value={song.price} onChange={handleChange} placeholder = 'Price...'/>
+
+        <label htmlFor="audio">audio</label>
+        <input type="file" id="audio" name="audio" accept="audio/*"></input>
+        <input className='addInput' type="text" name="audio" value={song.audio} onChange={handleChange} placeholder='Audio file...' />
+
         <br />
         <br />
 
-        <input className = 'submitButton' type="submit" />
-    </form>
+        <input className='submitButton' type="submit" />
+      </form>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './contexts/AuthProvider'
 import {BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
